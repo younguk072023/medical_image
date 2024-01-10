bc760c083b3ead470b87fd60e01686e5662d92cb
박영욱 <113082676+younguk072023@users.noreply.github.com>
2024-01-10 18:22:14 +0900

    Update 적맥.py

--- 적맥.py
+++ 적맥.py
@@ -17,22 +17,26 @@ gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 clahe_image = clahe.apply(gray)
 
-# 이미지 히스토그램 평활화
-equ = cv2.equalizeHist(gray)
+# 히스토그램 평활화
+equ = cv2.equalizeHist(clahe_image)
 
 # 가우시안 블러로 이미지 필터링
 blurred = cv2.GaussianBlur(equ, (5, 5), 0)
 
 # 이진화
-_, binary = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV)
+_, binary = cv2.threshold(blurred, 65, 255, cv2.THRESH_BINARY_INV)
 
 # 에지 검출 (Canny 사용)
 edges = cv2.Canny(binary, 30, 70)
 
 # 결과 이미지 출력
 cv2.imshow('Resized Image', resized_image)
+cv2.imshow('Gray Image', gray)
 cv2.imshow('CLAHE Image', clahe_image)
-
+cv2.imshow('Histogram Equalized Image', equ)
+cv2.imshow('Blurred Image', blurred)
+cv2.imshow('Binary Image', binary)
+cv2.imshow('Edges Image', edges)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
